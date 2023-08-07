7f5c0cd54f6f399c611f763e22df589cdbfd4467
ALFI ALHUZWIRI SYAM <223970007@bukopin.co.id>
2023-08-07 14:33:32 +0700

    update-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewsComponent } from './news/news.component';
+import { AppRoutingModule } from './admin/app-routing.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
